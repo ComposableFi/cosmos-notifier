046ba43c0e06fe5b30d96d8da2acc132b7f31f81
Robert Jonczy <robert.jonczy@gmail.com>
2024-04-12 11:17:51 +0200

    replace logging with logrus

--- go.mod
+++ go.mod
@@ -7,5 +7,7 @@ require (
 	github.com/moul/http2curl v1.0.0 // indirect
 	github.com/parnurzeal/gorequest v0.3.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
 	golang.org/x/net v0.24.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,18 +1,46 @@
 package main
 
 import (
-	"fmt"
 	"os"
+	"strings"
 	"time"
+
+	log "github.com/sirupsen/logrus"
 )
+
+const defaultInterval time.Duration = time.Minute
+
+func setLogLevel() {
+	level, exists := os.LookupEnv("LOG_LEVEL")
+	if !exists {
+		log.Info("LOG_LEVEL not set, defaulting to warning")
+		log.SetLevel(log.WarnLevel)
+		return
+	}
+
+	parsedLevel, err := log.ParseLevel(strings.ToLower(level))
+	if err != nil {
+		log.Warnf("Invalid LOG_LEVEL '%s', defaulting to warning", level)
+		log.SetLevel(log.WarnLevel)
+		return
+	}
+
+	log.SetLevel(parsedLevel)
+	log.Infof("Logging level set to %s", parsedLevel)
+}
+
+func init() {
+
+	log.SetOutput(os.Stdout)
+	setLogLevel()
+}
 
 func main() {
 
 	requiredVars := []string{"COSMOS_ENDPOINT", "SLACK_WEBHOOK_URL", "CHAIN_ID"}
 	for _, varName := range requiredVars {
 		if os.Getenv(varName) == "" {
-			fmt.Printf("Error: %s is not defined\n", varName)
-			os.Exit(1)
+			log.Panicf("Error: %s is not defined\n", varName)
 		}
 	}
 
@@ -22,31 +50,29 @@ func main() {
 
 	interval, err := time.ParseDuration(os.Getenv("FETCH_INTERVAL"))
 	if err != nil {
-		fmt.Printf("Error parsing FETCH_INTERVAL: %s\n", err)
+		log.Errorf("Error parsing FETCH_INTERVAL: %s, defaulting to 1m\n", err)
+		interval = defaultInterval
 		return
 	}
 
-	runsCounter := 0
-	lastPostedProposalId := ""
+	var lastProposalId string
 	for {
 		latestProposal, err := getLatestProposal(cosmosEndpoint)
 		if err != nil {
-			fmt.Printf("Error fetching proposal: %s\n", err)
+			log.Errorf("Error fetching proposal: %s\n", err)
 			continue
 		}
-		// post to slack only if
-		// not posted before
-		// and this is at least second iteration
-		if runsCounter > 0 && latestProposal.ID == lastPostedProposalId {
+
+		// post to slack only if didn't posted before
+		if lastProposalId != latestProposal.ID {
 			err = postToSlack(chainID, *latestProposal, slackWebhookURL)
 			if err != nil {
-				fmt.Printf("Error posting to slack: %s\n", err)
+				log.Errorf("Error posting to slack: %s\n", err)
 				continue
 			}
-			lastPostedProposalId = latestProposal.ID
+			lastProposalId = latestProposal.ID
 		}
 
 		time.Sleep(interval)
-		runsCounter++
 	}
 }
